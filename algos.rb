a6847212c75c0f8efdb8eed7e9c0c7042a1b5d6a
vojma <vojtech.matous@juicymo.cz>
2023-12-03 21:34:56 +0100

    Add hunt_and_target algo

--- algos.rb
+++ algos.rb
@@ -32,4 +32,61 @@ module Algos
       end
     end
   end
+
+  def hunt_and_target(api)
+    target_mode = false
+    last_hit = nil
+
+    loop do
+      row, col = if target_mode && last_hit
+                   find_next_target(api.grid, last_hit)
+                 else
+                   find_random_target(api.grid)
+                 end
+
+      response = api.fire(row, col)
+
+      if response["result"]
+        target_mode = true
+        last_hit = [row, col]
+        puts "Hit at #{row}, #{col}"
+      else
+        target_mode = false if target_mode && !adjacent_cells_hit?(api.grid, last_hit)
+        puts "Miss at #{row}, #{col}"
+      end
+
+      if response["finished"]
+        puts "Game over in #{response["moveCount"]} moves"
+        api.print_grid(response["grid"])
+        break
+      end
+    end
+  end
+
+  def find_next_target(grid, last_hit)
+    row, col = last_hit
+    adjacent_cells = [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]]
+
+    target = adjacent_cells.shuffle.find { |r, c| valid_target?(grid, r, c) }
+    target || find_random_target(grid)
+  end
+
+  def find_random_target(grid)
+    loop do
+      row = rand(Constants::GRID_SIZE)
+      col = rand(Constants::GRID_SIZE)
+      return [row, col] if valid_target?(grid, row, col)
+    end
+  end
+
+  def adjacent_cells_hit?(grid, cell)
+    row, col = cell
+    [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]].any? do |r, c|
+      grid[r][c] == 'X' if r.between?(0, Constants::GRID_SIZE - 1) && c.between?(0, Constants::GRID_SIZE - 1)
+    end
+  end
+
+  def valid_target?(grid, row, col)
+    row.between?(0, Constants::GRID_SIZE - 1) && col.between?(0, Constants::GRID_SIZE - 1) && (grid[row][col] == '*' || grid[row][col] == 'S')
+  end
 end
