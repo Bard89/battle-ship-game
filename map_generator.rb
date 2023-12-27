8ded88f694f929d0d5a62bef994c62b5488f5519
vojma <vojtech.matous@gmail.com>
2023-12-27 02:01:59 +0100

    Refactor map generator

--- map_generator.rb
+++ map_generator.rb
@@ -15,17 +15,10 @@ class MapGenerator
     place_ships
   end
 
-  private
-
   def place_ships
-    # Place the irregular ship first ( takes the most space)
     place_irregular_ship
-
-    # Place the regular ships after placing the irregular ship
     place_regular_ships
-
-    puts
-    puts "Final grid:"
+    puts "\nFinal grid:"
     print_grid(@grid.flatten.join(''))
   end
 
@@ -33,7 +26,7 @@ class MapGenerator
     placed = false
 
     until placed
-      row, col = rand(Constants::GRID_SIZE - 2), rand(Constants::GRID_SIZE - 4) # Adjust for ship size
+      row, col = rand(Constants::GRID_SIZE - 2), rand(Constants::GRID_SIZE - 4)
       ship_shape = [IRREGULAR_SHIP_HORIZONTAL, IRREGULAR_SHIP_VERTICAL].sample
 
       if can_place_whole_ship?(row, col, ship_shape)
@@ -43,50 +36,26 @@ class MapGenerator
     end
   end
 
+  def can_place_whole_ship?(row, col, ship_shape)
+    ship_shape.each_with_index do |ship_row, r|
+      ship_row.each_with_index do |cell, c|
+        # Check grid boundaries plus if the cell is already occupied ( can't happen now since we are placing the irregular ship first)
+        return false unless valid_coordinates?(row + r, col + c) && (cell != 'S' && cell != 'I' || grid[row + r][col + c] == '*')
+      end
+    end
+    true
+  end
+
   def place_regular_ships
-    REGULAR_SHIPS.each_with_index do |ship_size, index|
+    REGULAR_SHIPS.each do |ship_size|
       placed = false
 
       until placed
         row, col, horizontal = rand(Constants::GRID_SIZE), rand(Constants::GRID_SIZE), [true, false].sample
         next unless can_place_regular_ship?(row, col, ship_size, horizontal)
 
-        ship_size.times do |i|
-          if horizontal
-            grid[row][col + i] = 'S'
-          else
-            grid[row + i][col] = 'S'
-          end
-        end
-
-        # uncomment to see the grid after each ship is placed
-        #
-        puts
-        puts "Placed ship n. #{index} on row: #{row}, column: #{col} of size #{ship_size}, #{horizontal ? 'horizontally' : 'vertically'}"
-        print_grid(@grid.flatten.join(''))
-
+        ship_size.times { |i| horizontal ? grid[row][col + i] = 'S' : grid[row + i][col] = 'S' }
         placed = true
-      end
-    end
-  end
-
-  def can_place_whole_ship?(row, col, ship_shape)
-    ship_shape.each_with_index do |ship_row, r|
-      ship_row.each_with_index do |cell, c|
-        # Check grid boundaries
-        return false unless valid_coordinates?(row + r, col + c)
-        # Check if the cell is already occupied ( can't happen now since we are placing the irregular ship first)
-        return false if @grid[row + r][col + c] != '*' && (cell == 'S' || cell == 'I')
-      end
-    end
-
-    true
-  end
-
-  def place_whole_ship(row, col, ship_shape)
-    ship_shape.each_with_index do |ship_row, r|
-      ship_row.each_with_index do |cell, c|
-        grid[row + r][col + c] = cell if (cell == 'S' || cell == 'I')
       end
     end
   end
@@ -95,24 +64,20 @@ class MapGenerator
   def can_place_regular_ship?(row, col, ship_size, horizontal)
     if horizontal
       return false if col + ship_size > Constants::GRID_SIZE
+
       ship_cells = (col...(col + ship_size)).map { |c| [row, c] }
     else
       return false if row + ship_size > Constants::GRID_SIZE
+
       ship_cells = (row...(row + ship_size)).map { |r| [r, col] }
     end
 
-    surrounding_cells = ship_cells.flat_map do |r, c|
-      [
-        [r - 1, c], [r + 1, c],    # cells above and below
-        [r, c - 1], [r, c + 1],    # cells to the left and right
-        [r - 1, c - 1], [r + 1, c + 1], # diagonal cells
-        [r + 1, c - 1], [r - 1, c + 1]
-      ]
-    end.uniq
+    surrounding_cells = ship_cells.flat_map { |r, c| surrounding_cells(r, c) }.uniq
 
     # Check all cells for ship placement and surrounding for gaps
     (ship_cells + surrounding_cells).all? do |r, c|
-      next true if r < 0 || r >= Constants::GRID_SIZE || c < 0 || c >= Constants::GRID_SIZE # ignore out of bounds
+      next true unless valid_coordinates?(r, c)
+
       @grid[r][c] == '*'
     end
   end
@@ -122,6 +87,14 @@ class MapGenerator
     (-1..1).flat_map do |row_offset|
       (-1..1).map do |col_offset|
         [row + row_offset, col + col_offset]
+      end
+    end
+  end
+
+  def place_whole_ship(row, col, ship_shape)
+    ship_shape.each_with_index do |ship_row, r|
+      ship_row.each_with_index do |cell, c|
+        grid[row + r][col + c] = cell if (cell == 'S' || cell == 'I')
       end
     end
   end
