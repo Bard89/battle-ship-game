6fe99497e8e375f2b563c5d4e8c71478b9c036d3
vojma <vojtech.matous@gmail.com>
2023-12-28 01:12:45 +0100

    Itterate the ShipSunkOrNotProbability module

--- algos/modified_probability_density/ship_sunk_or_not_probability.rb
+++ algos/modified_probability_density/ship_sunk_or_not_probability.rb
@@ -1,88 +1,147 @@
-# This module is responsible for tracking ship hits and determining if ships have been sunk in the game of Battleship.
-# It manages both regular and irregular ships, updating the probability grid based on hits and sunken ships.
 module ShipSunkOrNotProbability
 
-  # Initializes the tracking of hits on ships. It sets up a hash to track hits on ships and an array to keep track of confirmed sunk ships.
   def initialize_ship_tracking
-    @hit_ships = {} # Hash to track hits on ships, with each ship's hits stored as an array of coordinates.
-    @confirmed_sunk_ships = [] # Array to keep track of confirmed sunk ships.
+    @hit_ships = {} # Key: Ship size or nil, Value: Array of hit coordinates
+    @confirmed_sunk_ships = {} # Tracks sunk ships by size or 'irregular'
   end
 
-  # Updates the ship sinking status based on the latest hit.
-  # @param probability_grid [Array] The current probability grid of the game.
-  # @param target_row [Integer] The row index of the target cell.
-  # @param target_col [Integer] The column index of the target cell.
-  # @param result [Boolean] The result of the hit attempt (true if hit, false otherwise).
-  def update_ship_sunk_or_not(probability_grid, target_row, target_col, result)
+  def update_ship_sunk_or_not(probability_grid, target_row, target_col, result, api)
     if result
-      ship_hits = record_hit(target_row, target_col)
+      ship_hits, ship_size = record_hit(target_row, target_col)
 
       if ship_sunk?(ship_hits, probability_grid)
-        update_for_sunk_ship(ship_hits, probability_grid)
+        ship_size = ship_size.nil? ? infer_ship_size(ship_hits) : ship_size
+        update_for_sunk_ship(ship_hits, ship_size, probability_grid)
 
-        @confirmed_sunk_ships.concat(ship_hits)
-        @hit_ships.delete(ship_hits.object_id)
+        @confirmed_sunk_ships[ship_size] = ship_hits
+        @hit_ships.delete(ship_size)
+        puts "Ship sunk: #{ship_size}"
       end
     end
+
+    process_avenger_availability(api) if api.avengerAvailable
   end
 
-  # Records a hit on a ship and checks if it connects to an existing ship.
-  # @param row [Integer] The row index of the hit cell.
-  # @param col [Integer] The column index of the hit cell.
-  # @return [Array] An array of hit coordinates for a particular ship.
   def record_hit(row, col)
-    @hit_ships.each do |hits|
+    new_hit = [row, col]
+
+    # Avoid adding a hit that's already been recorded
+    return if @hit_ships.any? { |_, hits| hits.include?(new_hit) }
+
+    # Check if the hit connects to an existing ship
+    @hit_ships.each do |size, hits|
       if hits.any? { |hit_row, hit_col| adjacent?(hit_row, hit_col, row, col) }
-        hits << [row, col]
-        return hits
+        hits << new_hit
+        return [hits, size]
       end
     end
-    new_ship_hits = [[row, col]]
-    @hit_ships[new_ship_hits.object_id] = new_ship_hits
-    new_ship_hits
+
+    # Start tracking a new ship if it's a new hit
+    @hit_ships[nil] ||= []
+    @hit_ships[nil] << new_hit
+    [@hit_ships[nil], nil]
+
+    # Ensure that ship_hits is never nil
+    ship_hits = @hit_ships.find { |_, hits| hits.include?([row, col]) }&.last
+    ship_hits ||= [] # If ship_hits is nil, initialize it as an empty array
+    ship_hits << [row, col] unless ship_hits.include?([row, col])
+
+    [ship_hits, nil] # Return ship_hits and nil for ship_size as it's unknown yet
   end
 
-  # Checks if two cells are adjacent (excluding diagonals).
-  # @param row1 [Integer] Row index of the first cell.
-  # @param col1 [Integer] Column index of the first cell.
-  # @param row2 [Integer] Row index of the second cell.
-  # @param col2 [Integer] Column index of the second cell.
-  # @return [Boolean] True if cells are adjacent, false otherwise.
+
   def adjacent?(row1, col1, row2, col2)
     (row1 == row2 && (col1 - col2).abs == 1) || (col1 == col2 && (row1 - row2).abs == 1)
   end
 
-  # Determines if a ship is sunk based on the current hits.
-  # @param ship_hits [Array] An array of hit coordinates for a ship.
-  # @param probability_grid [Array] The current probability grid of the game.
-  # @return [Boolean] True if the ship is sunk, false otherwise.
   def ship_sunk?(ship_hits, probability_grid)
-    ship_hits.all? do |hit_row, hit_col|
-      adjacent_cells(hit_row, hit_col).all? do |adj_row, adj_col|
-        probability_grid[adj_row][adj_col] == 0 || @confirmed_sunk_ships.include?([adj_row, adj_col])
-      end
+    return false if ship_hits.nil? || ship_hits.empty?
+
+    inferred_size = infer_ship_size(ship_hits)
+    return false if ship_hits.uniq.size != inferred_size
+
+    ship_hits.sort_by { |hit| [hit[0], hit[1]] }.each_cons(2) do |hit1, hit2|
+      # Check if hits are adjacent and in the same row or column
+      next unless (hit1[0] == hit2[0] && (hit1[1] - hit2[1]).abs == 1) ||
+        (hit1[1] == hit2[1] && (hit1[0] - hit2[0]).abs == 1)
+
+      # Check surrounding cells of the ship
+      return false unless surrounding_cells_sunk_or_missed(hit1, hit2, probability_grid)
     end
+
+    true
   end
 
-  # Retrieves a list of adjacent cells for a given cell.
-  # @param row [Integer] The row index of the cell.
-  # @param col [Integer] The column index of the cell.
-  # @return [Array] An array of adjacent cell coordinates.
+
+  def surrounding_cells_sunk_or_missed(hit1, hit2, probability_grid)
+    # Define the range of rows and columns to check based on hit positions
+    row_range = [hit1[0], hit2[0]].min..[hit1[0], hit2[0]].max
+    col_range = [hit1[1], hit2[1]].min..[hit1[1], hit2[1]].max
+
+    row_range.each do |row|
+      col_range.each do |col|
+        next if [row, col] == hit1 || [row, col] == hit2 # Skip the hit cells
+        return false unless probability_grid[row][col] == 0
+      end
+    end
+
+    true
+  end
+
+
   def adjacent_cells(row, col)
     [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]].select do |adj_row, adj_col|
       valid_coordinates?(adj_row, adj_col)
     end
   end
 
-  # Updates the probability grid when a ship is sunk.
-  # @param ship_hits [Array] An array of hit coordinates for the sunk ship.
-  # @param probability_grid [Array] The current probability grid of the game.
-  def update_for_sunk_ship(ship_hits, probability_grid)
+  def update_for_sunk_ship(ship_hits, ship_size, probability_grid)
     ship_hits.each do |hit_row, hit_col|
       probability_grid[hit_row][hit_col] = 0
       adjacent_cells(hit_row, hit_col).each do |adj_row, adj_col|
-        probability_grid[adj_row][adj_col] = 0 unless @confirmed_sunk_ships.include?([adj_row, adj_col])
+        probability_grid[adj_row][adj_col] = 0 unless @confirmed_sunk_ships.values.flatten(1).include?([adj_row, adj_col])
       end
     end
+    update_ship_size(ship_hits, ship_size) if ship_size.nil?
+  end
+
+  def update_ship_size(ship_hits, ship_size)
+    @hit_ships[ship_size] = ship_hits
+    @hit_ships.delete(nil)
+  end
+
+  def process_avenger_availability(api)
+    # Mark the irregular ship as sunk when Avenger is available
+    @confirmed_sunk_ships['irregular'] = true
+    @hit_ships.delete('irregular')
+  end
+
+  def infer_ship_size(ship_hits)
+    return 0 if ship_hits.nil? || ship_hits.empty?
+
+    sorted_by_rows = ship_hits.sort_by { |hit| [hit[0], hit[1]] }
+    sorted_by_cols = ship_hits.sort_by { |hit| [hit[1], hit[0]] }
+
+    max_length_row = max_continuous_hits(sorted_by_rows)
+    max_length_col = max_continuous_hits(sorted_by_cols)
+
+    [max_length_row, max_length_col].max
+  end
+
+  def max_continuous_hits(sorted_hits)
+    max_length = 1
+    current_length = 1
+
+    (1...sorted_hits.size).each do |i|
+      if sorted_hits[i][0] == sorted_hits[i - 1][0] && (sorted_hits[i][1] - sorted_hits[i - 1][1]).abs == 1 ||
+        sorted_hits[i][1] == sorted_hits[i - 1][1] && (sorted_hits[i][0] - sorted_hits[i - 1][0]).abs == 1
+        current_length += 1
+        max_length = [max_length, current_length].max
+      else
+        current_length = 1
+      end
+    end
+
+    max_length
   end
 end
